66923254c538a17be1950a7af644a5a6bc8d14b6
tsuyo <tsuyo@DESKTOP-BC07QQ1>
2020-10-23 22:02:47 -0500

    version5

--- src/main/java/co/edu/umanizales/listase/modelo/ListaDE.java
+++ src/main/java/co/edu/umanizales/listase/modelo/ListaDE.java
@@ -10,9 +10,9 @@ package co.edu.umanizales.listase.modelo;
  * @author tsuyo
  */
 public class ListaDE {
-    
+
     private NodoDE cabeza;
-    
+
     public ListaDE() {
     }
 
@@ -23,38 +23,37 @@ public class ListaDE {
     public void setCabeza(NodoDE cabeza) {
         this.cabeza = cabeza;
     }
-    
-    public int contarNodosDE(){
-        if(cabeza != null) {
+
+    public int contarNodosDE() {
+        if (cabeza != null) {
             NodoDE temp = cabeza;
             int cont = 1;
-            while (temp.getSiguiente() !=null) {
+            while (temp.getSiguiente() != null) {
                 temp = temp.getSiguiente();
                 cont++;
             }
             return cont;
-        }else {
-            
+        } else {
+
         }
         return 0;
     }
-    
-    
+
     public void adicionarNodo(Perro dato) {
         if (cabeza != null) {
             NodoDE temp = cabeza;
-            
+
             while (temp.getSiguiente() != null) {
                 temp = temp.getSiguiente();
             }
-            
+
             temp.setSiguiente(new NodoDE(dato));
             temp.getSiguiente().setAnterior(temp);
         } else {
             cabeza = new NodoDE(dato);
         }
     }
-    
+
     public void adicionarAlInicio(Perro dato) {
         if (cabeza != null) {
             NodoDE temp = new NodoDE(dato);
@@ -65,7 +64,7 @@ public class ListaDE {
             cabeza = new NodoDE(dato);
         }
     }
-    
+
     public void eliminarPorPosicion(int posicion) {
         if (cabeza != null) {
             if (posicion == 1) {
@@ -87,7 +86,7 @@ public class ListaDE {
             }
         }
     }
-    
+
     public void eliminar(Perro dato) {
         if (cabeza != null) {
             NodoDE temp = cabeza;
@@ -107,9 +106,9 @@ public class ListaDE {
             }
         }
     }
-    
-    public void invertir(){
-        if (cabeza != null){
+
+    public void invertir() {
+        if (cabeza != null) {
             ListaDE listaTemporal = new ListaDE();
             NodoDE temp = cabeza;
             while (temp != null) {
@@ -120,13 +119,56 @@ public class ListaDE {
             cabeza = listaTemporal.getCabeza();
         }
     }
-    
-    public void intercambiarExtremos(){
-        if (cabeza != null){
+
+    public void intercambiarExtremos() {
+        if (cabeza != null) {
             NodoDE temp = cabeza;
-            while (temp.getSiguiente()!= null && temp.getAnterior()!=null){
-                temp = temp.getAnterior();
+            while (temp.getSiguiente() != null && temp.getAnterior() != null) {
+                temp = temp.getAnterior().getSiguiente();
+
             }
+            Perro perrotemp = cabeza.getDato();
+            cabeza.setDato(temp.getDato());
+            temp.setDato(perrotemp);
+        }
+    }
+
+    public Perro encontrarxPosicionDE(int posicion) {
+        if (cabeza != null) {
+            NodoDE temp = cabeza;
+            int cont = 1;
+            while (posicion != cont) {
+                temp = temp.getSiguiente().getAnterior();
+                cont++;
+            }
+            return temp.getDato();
+        }
+        return null;
+    }
+
+    public void borrarPerroPorId(int id) {
+        if (cabeza != null) {
+            if (cabeza.getDato().getNumero() == id) {
+                NodoDE temp = cabeza.getSiguiente().getAnterior();
+                cabeza = temp;
+            } else {
+                NodoDE recienteDE, temp1;
+                recienteDE = cabeza;
+                temp1 = cabeza.getSiguiente().getAnterior();
+
+                while (temp1 != null && temp1.getDato().getNumero() != id) {
+                    recienteDE = recienteDE.getSiguiente().getAnterior();
+                    temp1 = temp1.getAnterior().getSiguiente();
+                }
+                if (temp1.getDato().getNumero() == id) {
+                    recienteDE.setSiguiente(temp1.getSiguiente());
+                    recienteDE.setAnterior(temp1.getAnterior());
+                    temp1 = temp1.getAnterior();
+                    temp1 = temp1.getSiguiente();
+                }
+            }
+        } else {
+            System.out.println("No hay dato para eliminar");
         }
     }
 }
